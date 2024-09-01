094d8024de10286b4a3599f5c5c1c64cafe16e12
Ravi Ranjan <ranjanravi5667@gmail.com>
2024-09-01 09:45:01 +0530

    themes updated

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -71,14 +71,6 @@ return {
 	},
 
 	-- ╭─────────────────────────────────────────────────────────╮
-	-- │ bamboo colorscheme                                      │
-	-- ╰─────────────────────────────────────────────────────────╯
-
-	{
-		"ribru17/bamboo.nvim",
-	},
-
-	-- ╭─────────────────────────────────────────────────────────╮
 	-- │ embark colorscheme                                      │
 	-- ╰─────────────────────────────────────────────────────────╯
 	{
@@ -98,12 +90,44 @@ return {
 
 	{
 		"roobert/palette.nvim",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			require("palette").setup({
+				palettes = {
+					main = "custom_main_palette",
+					accent = "pastel",
+					state = "pastel",
+				},
+
+				italics = true,
+				transparent_background = false,
+
+				custom_palettes = {
+					main = {
+						custom_main_palette = {
+							color0 = "#191D33",
+							color1 = "#1A1E39",
+							color2 = "#383f5e",
+							color3 = "#8491C3",
+							color4 = "#8D739B",
+							color5 = "#a7a9b7",
+							color6 = "#bdbfc9",
+							color7 = "#d3d4db",
+							color8 = "#e9e9ed",
+						},
+					},
+					accent = {},
+					state = {},
+				},
+			})
+		end,
 	},
 
 	-- ╭─────────────────────────────────────────────────────────╮
-	-- │ monochrome                                              │
+	-- │ sunbather colorscheme                                   │
 	-- ╰─────────────────────────────────────────────────────────╯
 	{
-		'kdheepak/monochrome.nvim',
-	}
+		"nikolvs/vim-sunbather",
+	},
 }
