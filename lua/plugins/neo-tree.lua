cf85bd0f56e8dbcc0280326451294a8a6747426d
RaviThakur1002 <ranjanravi5667@gmail.com>
2024-12-21 17:26:51 +0530

    neotree changes

--- lua/plugins/neo-tree.lua
+++ lua/plugins/neo-tree.lua
@@ -6,8 +6,18 @@ return {
     "nvim-tree/nvim-web-devicons",
     "MunifTanjim/nui.nvim",
   },
-
   config = function()
+    -- Your existing keymapping
     vim.keymap.set('n', '<leader>n', ':Neotree filesystem reveal left<CR>', {})
+
+    -- Add the Neo-tree setup configuration
+    require("neo-tree").setup({
+      filesystem = {
+        use_libuv_file_watcher = true,  -- This is the line you were looking for
+        follow_current_file = {
+          enabled = true,
+        },
+      },
+    })
   end
 }
