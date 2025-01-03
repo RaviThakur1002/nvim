9b11d45eb6694e2f0f9be471dee348341408a977
RaviThakur1002 <ranjanravi5667@gmail.com>
2025-01-04 01:23:14 +0530

    updated

--- lua/plugins/codesnap.lua
+++ lua/plugins/codesnap.lua
@@ -4,10 +4,16 @@ return {
 
 	config = function()
 		require("codesnap").setup({
-			--has_breadcrumbs = true,
-			watermark_font_family = "NATO Sans",
-			watermark = "devil003",
-			bg_theme = "peach",
+			title = "Devil003",
+			code_font_family = "CaskaydiaCove Nerd Font",
+			watermark_font_family = "CaskaydiaCove Nerd Font",
+			watermark = "Devil003", --water mark only works when padding is more like 122 or something
+			bg_theme = "bamboo",
+			breadcrumbs_separator = "/",
+			has_breadcrumbs = false,
+			has_line_number = false,
+			show_workspace = false,
+			min_width = 0,
 			bg_x_padding = 50,
 			bg_y_padding = 50,
 			save_path = "~/Pictures/Screenshots/",
